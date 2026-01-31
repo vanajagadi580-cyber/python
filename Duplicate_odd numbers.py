2312b57fc4aa59fefe0d5d32538a353e5844db39
vanajagadi580-cyber <vanajagadi580@gmail.com>
2026-01-31 10:53:03 +0530

    Create Duplicate_odd numbers.py

---
+++ Duplicate_odd numbers.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+for i in a:
+    if i%2==0:
+        b.append(i)
+    else:
+        b.append(i)
+        b.append(i)
+for i in b:
+    print(i,end=' ')
