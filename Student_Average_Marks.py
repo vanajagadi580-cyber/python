7244cea2717f6f2e2ca013bbd9fb24636e9ef24f
vanajagadi580-cyber <vanajagadi580@gmail.com>
2026-02-03 12:24:17 +0530

    Create Student_Average_Marks.py

---
+++ Student_Average_Marks.py
@@ -0,0 +1,11 @@
+sno=int(input("enter student no"))
+sname=input("enter name")
+group=input("enter group")
+s1=int(input("enter s1:"))
+s2=int(input("enter s2:"))
+s3=int(input("enter s3:"))
+s4=int(input("enter s4:"))
+total=s1+s2+s3+s4
+print(total)
+avg=total/3
+print(avg)
