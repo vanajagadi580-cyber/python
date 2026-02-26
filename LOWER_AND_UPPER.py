7462b9a4eb5dfb3da9eda691678b8c2b9ef175e5
vanajagadi580-cyber <vanajagadi580@gmail.com>
2026-02-26 19:54:57 +0530

    Create LOWER_AND_UPPER.py

---
+++ LOWER_AND_UPPER.py
@@ -0,0 +1,8 @@
+#LOWER CASE TO UPPER CASE
+num = int(input("Enter the value of num :"))
+amt = float(input("Enter the value of amt:"))
+pi = float(input("Enter the value of pi:"))
+code = str(input("Enter the value of code:"))
+msg =str(input("Enter the value of msg:"))
+#print the value of variable
+print("NUM= " + str(num) +"\n AMT=" + str(amt) +"\n PI=" +str(pi) +"\n CODE=" +str(code) +"\n MSG=" +str(msg))
